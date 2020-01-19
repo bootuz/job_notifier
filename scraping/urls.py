72660f6a0399a65f32a8da97550f6cdfb29c7b9e
Nart Kenobi <bootuz07@gmail.com>
2020-01-19 13:33:48 +0300

    some updates

---
+++ scraping/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from scraping import views
+
+urlpatterns = [
+    path('/scrape', views.scrape, name='scrape'),
+]
--- scraping/views.py
+++ scraping/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from telebot import TeleBot
+from telebot import TeleBot, types
 import os
 
 
@@ -12,3 +13,11 @@ bot = TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Welcome")
+
+
+def scrape(request):
+    json_str = request.body.decode('UTF-8')
+    update = types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+
+    return HttpResponse('success')
