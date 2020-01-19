be623669220e57ec37dbb234d554d9df9c887bf4
Nart Kenobi <bootuz07@gmail.com>
2020-01-19 14:22:30 +0300

    deleted / in index

--- scraping/urls.py
+++ scraping/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from scraping import views
 
 urlpatterns = [
-    path('/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('scrape/', views.scrape, name='scrape'),
 ]
