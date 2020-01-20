66f4ee379745d46f6a3e2c4d5593ff59386118bd
Nart Kenobi <bootuz07@gmail.com>
2020-01-20 15:32:58 +0300

    changed webhook

--- scraping/urls.py
+++ scraping/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 from scraping import views
 import os
 
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path(f'{os.environ["TOKEN"]}', views.scrape, name='scrape'),
+    path(f'{os.environ["TOKEN"]}', csrf_exempt(views.scrape), name='scrape'),
 ]
